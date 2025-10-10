f587eed7cdc0a68f46ba31de795129d7acefadf2
offsite.ryan <offsite.ryan@gmail.com>
2025-10-10 09:34:28 -0400

    fix: adjust column layout in console.chart for improved display of data groups

--- helper-functions.js
+++ helper-functions.js
@@ -325,7 +325,7 @@ console.chart = function (data, group = '-') {
     split[1] = `<span style="color:lime;">${split[1]}</span>`;
     split[3] = `<span style="color:red;">${split[3]}</span>`;
     // const htmlContent = replaceAll(msg,'%c',''); //.replace(/\n/g, '<br>');
-    preElement.innerHTML = `<span class="w3-padding w3-center w3-col ${group.startsWith('OPEN POSITIONS') ? 's12' : 's12 m6'}">${split.join('')}</span>`;
+    preElement.innerHTML = `<span class="w3-padding w3-center w3-col ${group.startsWith('OPEN POSITIONS') ? 's8' : (group.startsWith('ALL') ? 's12 m4' : 's12 m4')}">${split.join('')}</span>`;
     // preElement.innerHTML = split.join('');
 
     // Assuming you have an HTML element with id="output"
